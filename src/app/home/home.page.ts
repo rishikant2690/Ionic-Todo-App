edfc5de28188a0df8109e470ec596b4ffc981882
Developer2690 <45072765+Developer2690@users.noreply.github.com>
2021-03-29 00:14:02 +0530

    Ionic moble app

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {  IonContent, AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+public txtTodo = [];
+  constructor( private alertController: AlertController) {}
 
-  constructor() {}
+  async  openTodoAlert(){
+    const  addTodoAlert = await this.alertController.create({
+       // title:"Add a todo",
+        message :"Enter your todo",
+        inputs:[{
+          type : "text",
+          name:  "addTodoInput"
+        }],
+        buttons:[{
+          text:"Cancel"
+        },
+        {
+          text:"Add Todo",
+          handler:(inputData)=>{
+                 let todoText;
+                 todoText = inputData.addTodoInput;
+                 this.txtTodo.push(todoText);
+          }
+        }]
 
+      });
+      await  addTodoAlert.present();
+  }
+  
 }
